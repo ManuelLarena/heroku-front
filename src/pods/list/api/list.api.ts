96f561a9598f045603ffd0bfc7317a2860b8d8ed
Manuel <malaan71@hotmail.com>
2021-01-02 00:24:48 +0100

    add backend connection

--- src/pods/list/api/list.api.ts
+++ src/pods/list/api/list.api.ts
@@ -1,11 +1,16 @@
 import Axios from 'axios';
 import { Member } from './list.api-model';
 
+const url = `${process.env.BASE_API_URL}/members`
 export const getMemberList = async (
   organization: string
 ): Promise<Member[]> => {
   const { data } = await Axios.get(
-    `https://api.github.com/orgs/${organization}/members`
+    `${url}/${organization}`, {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      }
+    }
   );
   return data;
 };
