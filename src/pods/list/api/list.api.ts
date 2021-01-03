8bafd8f91cac6150b912f26070b402c217e1dda3
Manuel <malaan71@hotmail.com>
2021-01-03 21:46:39 +0100

    revert last changes

--- src/pods/list/api/list.api.ts
+++ src/pods/list/api/list.api.ts
@@ -1,16 +1,11 @@
 import Axios from 'axios';
 import { Member } from './list.api-model';
 
-const url = `${process.env.BASE_API_URL}/members`
 export const getMemberList = async (
   organization: string
 ): Promise<Member[]> => {
   const { data } = await Axios.get(
-    `${url}/${organization}`, {
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-      }
-    }
+    `https://api.github.com/orgs/${organization}/members`
   );
   return data;
 };
